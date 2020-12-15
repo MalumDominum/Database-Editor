1dcfc7aaaf0796d5d7f271775573dd6474cecbf2
Mihail Alekseev <mihailalekseev02@gmail.com>
2020-12-15 17:36:37 +0200

    Rewrote FillDs funtion using DataSet rather than my own class. Add  ContextMenuItems

--- DbCourseWork/DbCommunication.cs
+++ DbCourseWork/DbCommunication.cs
@@ -15,105 +15,31 @@ namespace DbCourseWork
     {
         public DataSet Ds { get; set; }
         public NpgsqlDataAdapter Adapter { get; set; }
-        public NpgsqlCommandBuilder Builder { get; set; }
-        public Dictionary<string, DataBase> DataBases { get; set; } = new Dictionary<string, DataBase>();
+        public NpgsqlConnectionStringBuilder ConnectionBuilder { get; set; }
 
-        public DbCommunication()
+        public DbCommunication(string dbName, string userId, string server, string port, string password)
         {
-            for (int i = 1; i < ConfigurationManager.ConnectionStrings.Count; i++)
-                DataBases.Add(ConfigurationManager.ConnectionStrings[i].Name,
-                    new DataBase(new NpgsqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings[i].ConnectionString)) );
+            ConnectionBuilder = new NpgsqlConnectionStringBuilder("Server=" + server + ";Port=" + port + ";")
+            {
+                {"Database", dbName},
+                {"User Id", userId}
+            };
+            ConnectionBuilder.Password = password;
+
+            Ds = new DataSet(dbName);
         }
 
-        public async Task ConnectionToPostgres()
+        public async Task ShowOnDataGrid(DataTable table)
         {
-            //@"Server=localhost;Port=5433;Database=streamingservice;User Id=postgres;Password=c788f928bd244746ae0ca3c79c3488a8;"
-            // For Ms SQL use SqlConnectionStringBuilder
-            var connectionBuilder = new NpgsqlConnectionStringBuilder("Server=localhost;Port=5433;")
-            {
-                {"Database", "streamingservice"}, 
-                {"User Id", "postgres"}
-            };
-            connectionBuilder.Password = "c788f928bd244746ae0ca3c79c3488a8";
-
             try
             {
-                using (var connection = new NpgsqlConnection(connectionBuilder.ConnectionString))
+                using (var connection = new NpgsqlConnection(ConnectionBuilder.ConnectionString))
                 {
+                    string sqlExpression = "SELECT * FROM " + table.TableName;
                     await connection.OpenAsync();
-                    var sqlExpression = "INSERT INTO persons (first_name, last_name, gender, birth_date, height)" +
-                    "VALUES('Райан', 'Гослинг', 'м', date '1980-11-12', 1.84)";
-                    var command = new NpgsqlCommand(sqlExpression, connection);
-                    int number = await command.ExecuteNonQueryAsync();
-                    MessageBox.Show("Добавлено объектов:" + number);
-                }
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        public async Task SelectPersons()
-        {
-            var builder = new NpgsqlConnectionStringBuilder("Server=localhost;Port=5433;")
-            {
-                {"Database", "streamingservice"},
-                {"User Id", "postgres"}
-            };
-            builder.Password = "c788f928bd244746ae0ca3c79c3488a8";
-
-            try
-            {
-                using (var connection = new NpgsqlConnection(builder.ConnectionString))
-                {
-                    await connection.OpenAsync();
-                    var sqlExpression = "SELECT * FROM persons";
-                    var command = new NpgsqlCommand(sqlExpression, connection);
-                    var reader = await command.ExecuteReaderAsync();
-                    if (reader.HasRows)
-                    {
-                        var rowNames = "";
-                        for (int i = 0; i < reader.FieldCount; i++)
-                            rowNames += reader.GetName(i) + " ";
-                        MessageBox.Show(rowNames);
-
-                        while (await reader.ReadAsync())
-                        {
-                            var row = "";
-                            for (int i = 0; i < reader.FieldCount; i++)
-                                row += reader.GetValue(i) + " ";
-                            MessageBox.Show(row);
-                        }
-                    }
-                    reader.Close();
-                }
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        public async Task ShowOnDataGrid()
-        {
-            var builder = new NpgsqlConnectionStringBuilder("Server=localhost;Port=5433;")
-            {
-                {"Database", "streamingservice"},
-                {"User Id", "postgres"}
-            };
-            builder.Password = "c788f928bd244746ae0ca3c79c3488a8";
-
-            try
-            {
-                using (var connection = new NpgsqlConnection(builder.ConnectionString))
-                {
-                    await connection.OpenAsync();
-                    var sqlExpression = "SELECT * FROM persons";
                     Adapter = new NpgsqlDataAdapter(sqlExpression, connection);
-                    Ds = new DataSet();
-                    Adapter.Fill(Ds);
-                    MainWindow.CustomGrid.DataContext = Ds.Tables[0];
+                    Adapter.Fill(Ds, table.TableName);
+                    MainWindow.CustomGrid.DataContext = Ds.Tables[table.TableName];
                 }
             }
             catch (SqlException ex)
@@ -122,25 +48,18 @@ namespace DbCourseWork
             }
         }
 
-        public async Task UpdateDb(string table)
+        public async Task UpdateDb(DataTable table)
         {
-            var builder = new NpgsqlConnectionStringBuilder("Server=localhost;Port=5433;")
-            {
-                {"Database", "streamingservice"},
-                {"User Id", "postgres"}
-            };
-            builder.Password = "c788f928bd244746ae0ca3c79c3488a8";
-
             try
             {
-                using (var connection = new NpgsqlConnection(builder.ConnectionString))
+                using (var connection = new NpgsqlConnection(ConnectionBuilder.ConnectionString))
                 {
                     await connection.OpenAsync();
-                    Adapter.Update(MainWindow.CustomGrid.DataContext as DataTable
-                                   ?? throw new InvalidOperationException("You don't choose a table"));
+                    var commandBuilder = new NpgsqlCommandBuilder(Adapter);
+                    Adapter.Update(Ds);
                     Ds.Clear();
                     Adapter.Fill(Ds);
-                    MainWindow.CustomGrid.DataContext = Ds.Tables[0];
+                    MainWindow.CustomGrid.DataContext = Ds.Tables["persons"];
                 }
             }
             catch (SqlException ex)
@@ -149,12 +68,11 @@ namespace DbCourseWork
             }
         }
 
-        public DataBase AddDbTableNames(DataBase db)
+        public void FillDs()
         {
-            var builder = db.ConnectionBuilder;
             try
             {
-                using (var connection = new NpgsqlConnection(builder.ConnectionString))
+                using (var connection = new NpgsqlConnection(ConnectionBuilder.ConnectionString))
                 {
                     connection.Open();
                     var sqlExpression = // Select all table names
@@ -169,17 +87,29 @@ namespace DbCourseWork
                     if (reader.HasRows)
                         while (reader.Read())
                             tableNames.Add(reader.GetValue(0).ToString());
-
-                    db.TableNames = tableNames;
-
                     reader.Close();
-                    return db;
+                    foreach (var table in tableNames.Select(tableName => new DataTable(tableName)))
+                    {
+                        sqlExpression = // Select all column names
+                            "SELECT column_name                          " +
+                            "FROM information_schema.columns             " +
+                            "WHERE table_name = '" + table.TableName + "'" +
+                            "ORDER BY ordinal_position";
+                        command.CommandText = sqlExpression;
+                        reader = command.ExecuteReader();
+                        if (!reader.HasRows) continue;
+                        while (reader.Read())
+                            table.Columns.Add(new DataColumn(reader.GetValue(0).ToString()));
+
+                        Ds.Tables.Add(table);
+
+                        reader.Close();
+                    }
                 }
             }
             catch (SqlException ex)
             {
                 MessageBox.Show(ex.Message);
-                return db;
             }
         }
     }
--- DbCourseWork/MainWindow.xaml.cs
+++ DbCourseWork/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
@@ -21,21 +22,27 @@ namespace DbCourseWork
     public partial class MainWindow : Window
     {
         public static DataGrid CustomGrid { get; private set; }
-        public static DbCommunication Communication { get; private set; }
+        public static Dictionary<string, DbCommunication> Communications { get; private set; } = new Dictionary<string, DbCommunication>();
+        public DataTable CurrentTable { get; set; }
         public MainWindow()
         {
             InitializeComponent();
             CustomGrid = DataGrid;
-            Communication = new DbCommunication();
-            Communication.ShowOnDataGrid().GetAwaiter();
+            Communications.Add("streamingservice", new DbCommunication("streamingservice","postgres",
+                "localhost", "5433", "c788f928bd244746ae0ca3c79c3488a8"));
+            Communications["streamingservice"].FillDs();
 
-            Communication.DataBases["streamingservice"] = Communication.AddDbTableNames(Communication.DataBases["streamingservice"]);
-            MessageBox.Show(Communication.DataBases["streamingservice"].TableNames.Count.ToString());
+            FillTreeView(DatabasesTreeView, Communications["streamingservice"]);
         }
 
         private void SaveChangesButton_OnClick(object sender, RoutedEventArgs e)
         {
-            Communication.UpdateDb("persons").GetAwaiter();
+            Communications["streamingservice"].UpdateDb(CurrentTable).GetAwaiter();
+        }
+
+        private void ReturnChangesButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            CustomGrid.DataContext = CurrentTable;
         }
 
         private void ArrowPressed_OnKeyDown(object sender, KeyEventArgs e)
@@ -76,6 +83,125 @@ namespace DbCourseWork
         private void DeleteMenuItem_OnClick(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void FillTreeView(TreeView treeView, DbCommunication communication)
+        {
+            var dbContextMenu = new ContextMenu();
+            var dbRefreshItem = new MenuItem { Header = "Refresh" };
+            dbRefreshItem.Click += DbMenuItem_Refresh;
+            dbContextMenu.Items.Add(dbRefreshItem);
+
+            var dbDisconnectItem = new MenuItem { Header = "Disconnect" };
+            dbDisconnectItem.Click += DbMenuItem_Disconnect;
+            dbContextMenu.Items.Add(dbDisconnectItem);
+
+
+            var tableContextMenu = new ContextMenu();
+            var tableViewAllItem = new MenuItem { Header = "View all rows" };
+            tableViewAllItem.Click += ViewAll_Click;
+            tableContextMenu.Items.Add(tableViewAllItem);
+
+            var tableViewFirstItem = new MenuItem { Header = "View first 100 rows" };
+            tableViewAllItem.Click += ViewFirst_Click;
+            tableContextMenu.Items.Add(tableViewFirstItem);
+
+            var tableViewLastItem = new MenuItem 
+            { 
+                Header = "View last 100 rows",
+                CommandParameter = 
+                    "{ Binding PlacementTarget," +
+                      "RelativeSource=" +
+                      "{" +
+                         "RelativeSource FindAncestor," +
+                         "AncestorType = { x:Type ContextMenu }" +
+                    "}}"
+            };
+            tableViewAllItem.Click += ViewLast_Click;
+            tableContextMenu.Items.Add(tableViewLastItem);
+
+            var tableViewFilteredItem = new MenuItem { Header = "View filter options" };
+            tableViewAllItem.Click += ViewFiltered_Click;
+            tableContextMenu.Items.Add(tableViewFilteredItem);
+
+            /*var columnContextMenu = new ContextMenu();
+            var columnMenuItem = new MenuItem { Header = "" };
+            //attributeMenuItem.Click += AttributeMenuItem_Click;
+            columnContextMenu.Items.Add(columnMenuItem);*/
+
+
+            var dbView = new TreeViewItem
+            {
+                Header = communication.Ds.DataSetName,
+                ContextMenu = dbContextMenu
+            };
+            treeView.Items.Add(dbView);
+
+            foreach (DataTable table in communication.Ds.Tables)
+            {
+                var tableView = new TreeViewItem
+                {
+                    Header = table.TableName,
+                    ContextMenu = tableContextMenu
+                };
+                dbView.Items.Add(tableView);
+
+                foreach (DataColumn column in table.Columns)
+                {
+                    var columnView = new TreeViewItem { Header = column.ColumnName };
+                    tableView.Items.Add(columnView);
+                }
+            }
+        }
+
+        private void DbMenuItem_Refresh(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void DbMenuItem_Disconnect(object sender, RoutedEventArgs e)
+        {
+            // Сделать серым в TreeView или удалить
+        }
+
+        private void ViewAll_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(e.OriginalSource is MenuItem menuItem)) return;
+            MessageBox.Show("1");
+            if (!(menuItem.CommandParameter is ContextMenu contextMenu)) return;
+            MessageBox.Show("2");
+            if (!(contextMenu.PlacementTarget is TreeViewItem viewItem)) return;
+
+            var parentControl = GetTreeViewItemParent(viewItem);
+            var parentView = parentControl as TreeViewItem;
+
+            var dbHeader = parentView.Header.ToString();
+            CurrentTable = Communications[dbHeader].Ds.Tables[viewItem.Header.ToString()];
+            Communications[dbHeader].ShowOnDataGrid(CurrentTable).GetAwaiter();
+        }
+
+        private void ViewFirst_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ViewLast_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ViewFiltered_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public ItemsControl GetTreeViewItemParent(TreeViewItem item)
+        {
+            var parent = VisualTreeHelper.GetParent(item);
+            while (!(parent is TreeViewItem || parent is TreeView))
+                parent = VisualTreeHelper.GetParent(parent);
+
+            return parent as ItemsControl;
         }
     }
 }
